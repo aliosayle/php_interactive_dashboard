06d5b7b88ac3c9136358e7165a5b79c5111cd2de
= <aliossaily24@gmail.com>
2024-12-02 16:23:59 +0100

    removed search bar

--- Admin/layouts/vertical-menu.php
+++ Admin/layouts/vertical-menu.php
@@ -28,12 +28,12 @@
             </button>
 
             <!-- App Search-->
-            <form class="app-search d-none d-lg-block">
+            <!-- <form class="app-search d-none d-lg-block">
                 <div class="position-relative">
-                    <input type="text" class="form-control" placeholder="<?php echo $language["Search"]; ?>">
+                    <input type="text" class="form-control" placeholder="search">
                     <button class="btn btn-primary" type="button"><i class="bx bx-search-alt align-middle"></i></button>
                 </div>
-            </form>
+            </form> -->
         </div>
 
         <div class="d-flex">
