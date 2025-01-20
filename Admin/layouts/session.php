434af682b610838aa6377f6d247349745d00f496
= <aliossaily24@gmail.com>
2025-01-20 11:47:43 +0100

    bug fixes

--- Admin/layouts/session.php
+++ Admin/layouts/session.php
@@ -1,12 +1,14 @@
 <?php
+// Set a unique session name for this system
+session_name("bon_system"); // Change "system1_session" to something unique for each system
+
 // Initialize the session
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+// Check if the user is logged in, if not then redirect them to the login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: auth-login.php");
     exit;
 }
